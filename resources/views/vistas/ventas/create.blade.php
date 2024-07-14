8e9c313a9734d75e44f5c38b7b9635cbc4c453cc
Alusheismael <alejolede605@gmail.com>
2024-07-13 21:46:48 -0500

    actualizacion de registro movimientos

--- resources/views/vistas/ventas/create.blade.php
+++ resources/views/vistas/ventas/create.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
 <link rel="stylesheet" href="{{ asset('css/ventas.css') }}">
-<div class="container mx-auto mt-5">
-    <div class="bg-white shadow-md rounded-lg">
+<div class="mx-auto mt-5">
+    <div class="h-1/4 bg-white shadow-md rounded-lg">
         <div class="bg-blue-500 text-white p-4 rounded-t-lg">
             <h4 class="text-xl font-semibold">+ Nueva Factura</h4>
         </div>
@@ -46,40 +46,50 @@
                     <button type="button" class="btn btn-product bg-blue-500 text-white py-2 px-4 rounded-md" onclick="openProductModal()">+ Nuevo producto</button>
                     <button type="button" class="bg-teal-500 text-white py-2 px-4 rounded-md" onclick="guardarVenta()">Guardar</button>
                 </div>
-                <table class="min-w-full divide-y divide-gray-200 mt-6">
-                    <thead class="bg-gray-50">
-                        <tr>
-                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">CODIGO</th>
-                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">CANT.</th>
-                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">DESCRIPCION</th>
-                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">ENVASE</th>
-                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">PRECIO UNIT.</th>
-                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">PRECIO TOTAL</th>
-                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Acciones</th>
-                        </tr>
-                    </thead>
-                    <tbody id="productsTable" class="bg-white divide-y divide-gray-200">
-                        <!-- Productos añadidos aparecerán aquí -->
-                    </tbody>
-                    <tfoot class="bg-gray-50">
-                        <tr>
-                            <td colspan="6" class="px-6 py-3 text-right text-sm font-medium text-gray-500">SUBTOTAL $</td>
-                            <td class="px-6 py-3 text-sm font-medium text-gray-900" id="subtotal">0.00</td>
-                        </tr>
-                        <tr>
-                            <td colspan="6" class="px-6 py-3 text-right text-sm font-medium text-gray-500">DESCUENTO $</td>
-                            <td class="px-6 py-3 text-sm font-medium text-gray-900" id="descuento">0.00</td>
-                        </tr>
-                        <tr>
-                            <td colspan="6" class="px-6 py-3 text-right text-sm font-medium text-gray-500">IVA (15%) $</td>
-                            <td class="px-6 py-3 text-sm font-medium text-gray-900" id="iva">0.00</td>
-                        </tr>
-                        <tr>
-                            <td colspan="6" class="px-6 py-3 text-right text-sm font-medium text-gray-500">TOTAL $</td>
-                            <td class="px-6 py-3 text-sm font-medium text-gray-900" id="total">0.00</td>
-                        </tr>
-                    </tfoot>
-                </table>
+                <div>
+                    <table class="min-w-full divide-y divide-gray-200 mt-6">
+                        <thead class="bg-gray-50">
+                            <tr>
+                                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">CODIGO</th>
+                                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">CANT.</th>
+                                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">DESCRIPCION</th>
+                                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">ENVASE</th>
+                                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">PRECIO UNIT.</th>
+                                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">PRECIO TOTAL</th>
+                                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Acciones</th>
+                            </tr>
+                        </thead>
+                    </table>
+                    <div class="overflow-y-auto" style="max-height: 180px;">
+                        <table class="min-w-full divide-y divide-gray-200">
+                            <tbody id="productsTable" class="bg-white divide-y divide-gray-200">
+                                <!-- Productos añadidos aparecerán aquí -->
+                            </tbody>
+                        </table>
+                    </div>
+                    <table class="min-w-full divide-y divide-gray-200">
+                        <tfoot class="bg-gray-50">
+                            <tr>
+                                <td colspan="6" class="px-6 py-3 text-right text-sm font-medium text-gray-500">SUBTOTAL $</td>
+                                <td class="px-6 py-3 text-sm font-medium text-gray-900" id="subtotal">0.00</td>
+                            </tr>
+                            <tr>
+                                <td colspan="6" class="px-6 py-3 text-right text-sm font-medium text-gray-500">DESCUENTO $</td>
+                                <td class="px-6 py-3 text-sm font-medium text-gray-900" id="descuento">0.00</td>
+                            </tr>
+                            <tr>
+                                <td colspan="6" class="px-6 py-3 text-right text-sm font-medium text-gray-500">IVA (15%) $</td>
+                                <td class="px-6 py-3 text-sm font-medium text-gray-900" id="iva">0.00</td>
+                            </tr>
+                            <tr>
+                                <td colspan="6" class="px-6 py-3 text-right text-sm font-medium text-gray-500">TOTAL $</td>
+                                <td class="px-6 py-3 text-sm font-medium text-gray-900" id="total">0.00</td>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
+
+
             </form>
         </div>
     </div>
@@ -116,82 +126,88 @@
                 });
             });
     }
+
     function guardarVenta() {
-    const productos = [];
+        const productos = [];
 
-    document.querySelectorAll('#productsTable tr').forEach(row => {
-        const art_id = row.querySelector('td:nth-child(1)').textContent;
-        const det_unidades = row.querySelector('td:nth-child(2) input').value;
-        const det_precio = row.querySelector('td:nth-child(5)').textContent;
-        const det_total = row.querySelector('td:nth-child(6)').textContent;
-        const art_envase = row.querySelector('td:nth-child(4) input').checked ? true : false;
+        document.querySelectorAll('#productsTable tr').forEach(row => {
+            const art_id = row.querySelector('td:nth-child(1)').textContent;
+            const det_unidades = row.querySelector('td:nth-child(2) input').value;
+            const det_precio = row.querySelector('td:nth-child(5)').textContent;
+            const det_total = row.querySelector('td:nth-child(6)').textContent;
+            const art_envase = row.querySelector('td:nth-child(4) input').checked ? true : false;
 
-        const producto = {
-            art_id: art_id,
-            det_precio: det_precio,
-            det_unidades: det_unidades,
-            det_precio_total: det_total,
-            art_envase: art_envase
-        };
+            const producto = {
+                art_id: art_id,
+                det_precio: det_precio,
+                det_unidades: det_unidades,
+                det_precio_total: det_total,
+                art_envase: art_envase
+            };
 
-        productos.push(producto);
-    // console.log('Producto agregado:', producto); // Imprime cada producto agregado
-    });
+            productos.push(producto);
+        });
 
-// console.log('Todos los productos:', productos); // Imprime todos los productos agregados
-
-    const ventaData = {
-        cli_codigo: document.getElementById('cedula').value,
-        vent_total: parseFloat(document.getElementById('total').innerText),
-        vent_subtotal: parseFloat(document.getElementById('subtotal').innerText),
-        vent_fecha: document.getElementById('fecha').value,
-        fpa_id: document.getElementById('pago').value,
-
-        detalle_ventas: productos
-    };
-
-    // Imprimir ventaData en la consola
-    // console.log(ventaData);
-
-    fetch('/vistas/ventas', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
-            },
-            body: JSON.stringify(ventaData)
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                console.log(data);
-                Swal.fire({
-                    title: 'Éxito',
-                    text: data.message,
-                    icon: 'success',
-                    confirmButtonText: 'Aceptar'
-                }).then(() => {
-                    window.location.href = '/vistas/ventas';
-                });
-            } else {
-                Swal.fire({
-                    title: 'Error',
-                    text: data.message,
-                    icon: 'error',
-                    confirmButtonText: 'Aceptar'
-                });
-            }
-        })
-        .catch(error => {
+        if (productos.length === 0) {
             Swal.fire({
                 title: 'Error',
-                text: 'Hubo un error al guardar la venta',
+                text: 'Debe agregar al menos un producto a la venta',
                 icon: 'error',
                 confirmButtonText: 'Aceptar'
             });
-        });
-}
+            return;
+        }
 
+        const ventaData = {
+            cli_codigo: document.getElementById('cedula').value,
+            vent_total: parseFloat(document.getElementById('total').innerText),
+            vent_subtotal: parseFloat(document.getElementById('subtotal').innerText),
+            vent_fecha: document.getElementById('fecha').value,
+            fpa_id: document.getElementById('pago').value,
+            detalle_ventas: productos
+        };
+
+        // Imprimir ventaData en la consola
+        console.log(ventaData);
+
+        fetch('/vistas/ventas', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+                },
+                body: JSON.stringify(ventaData)
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    console.log(data);
+                    Swal.fire({
+                        title: 'Éxito',
+                        text: data.message,
+                        icon: 'success',
+                        confirmButtonText: 'Aceptar'
+                    }).then(() => {
+                        window.location.href = '/vistas/ventas';
+                    });
+                } else {
+                    Swal.fire({
+                        title: 'Error',
+                        text: data.message,
+                        icon: 'error',
+                        confirmButtonText: 'Aceptar'
+                    });
+                }
+            })
+            .catch(error => {
+                Swal.fire({
+                    title: 'Error',
+                    text: 'Hubo un error al guardar la venta',
+                    icon: 'error',
+                    confirmButtonText: 'Aceptar'
+                });
+            });
+    }
 </script>
 
 <script src="{{ asset('js/ventas.js') }}"></script>
